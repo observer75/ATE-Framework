ba12aa9c83a4a164a99e219d15b7393438b48bf2
Olexander Berdychevsky <olexander.berdychevsky@hotmail.com>
2016-04-07 18:21:02 +0300

    18:20

--- src/framework/Firefox.java
+++ src/framework/Firefox.java
@@ -2,12 +2,10 @@ package framework;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import javax.mail.search.SubjectTerm;
-
 
 public class Firefox{
 	
