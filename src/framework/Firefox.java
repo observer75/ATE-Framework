e85a7f5ac512f693cc7020f2ea26cbc6812c78f1
Olexander Berdychevsky <olexander.berdychevsky@hotmail.com>
2016-04-07 16:36:38 +0300

    Compacted code

--- src/framework/Firefox.java
+++ src/framework/Firefox.java
@@ -3,54 +3,39 @@ package framework;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.interactions.*;
+import javax.mail.search.SubjectTerm;
+
 
 public class Firefox{
 	
-	String baseUrlF = "https://mail.google.com";
+	String baseUrl = "https://mail.google.com";
 	
 	WebDriver driverFirefox = new FirefoxDriver();
 	
-	//WebDriverWait waitForPassword = new WebDriverWait(driverFirefox, 30);
 	WebDriverWait wait = new WebDriverWait(driverFirefox, 20);
 	
 	public void Login(){
 		  
-		  driverFirefox.get(baseUrlF);
+		  driverFirefox.get(baseUrl);
 		  
-		  WebElement login = driverFirefox.findElement(By.id("Email"));
-		  login.sendKeys("iocmarus@gmail.com");
-		  
-		  WebElement next = driverFirefox.findElement(By.id("next"));
-		  next.click();
+		  driverFirefox.findElement(By.id("Email")).sendKeys("iocmarus@gmail.com");
+		  driverFirefox.findElement(By.id("next")).click();
 		  
 		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("Passwd")));
 
-		  WebElement passwd = driverFirefox.findElement(By.id("Passwd"));
-		  passwd.sendKeys("US!pf.avg");
-		  
-		  WebElement signIn = driverFirefox.findElement(By.id("signIn"));
-		  signIn.click();
+		  driverFirefox.findElement(By.id("Passwd")).sendKeys("US!pf.avg");
+		  driverFirefox.findElement(By.id("signIn")).click();
 	  }
 	
 	public void searchMessage(){
-		
 		  
 		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("gbqfq")));
 		  
-		  WebElement search = driverFirefox.findElement(By.id("gbqfq"));
-		  search.sendKeys("maven");
-		  
-		  WebElement searchButton = driverFirefox.findElement(By.id("gbqfb"));
-		  searchButton.click();
-		  
-		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(":ev")));
-		  
-		  WebElement message = driverFirefox.findElement(By.id(":ev"));
-		  message.click();
+		  driverFirefox.findElement(By.id("gbqfq")).sendKeys("maven");
+		  driverFirefox.findElement(By.id("gbqfb")).click();
+		  driverFirefox.findElement(By.id(":ex")).click();
 	}
 }
